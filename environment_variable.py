fc8c6ce9dc8eb87869f1b510638778204e337369
11609775 <vaibhavtryx@gmail.com>
2020-11-05 23:36:29 +0530

    My very First commit

---
+++ environment_variable.py
@@ -0,0 +1,18 @@
+import os
+
+db_user = os.environ.get('DB_USER')   
+db_password = os.environ.get('DB_USER') 
+
+print(db_user)
+print(db_password)
+
+
+## https://youtu.be/IolxqkL7cD8 ( for more detail )
+
+import os
+
+db_user = 'my_db_user'             ## fake database user
+db_password = 'my_db_pass_123!'    ## fake database password
+
+print(db_user)
+print(db_password)
